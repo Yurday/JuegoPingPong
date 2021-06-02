dd7448d1df7a3e7e3f7261b1a252cd1b9b5a6fbf
Cristhian <yurday93@hotmail.com>
2021-06-02 05:42:11 -0500

    Creación del constructor para la introducción de las barras en el juego de Ping Pong

--- main.js
+++ main.js
@@ -20,6 +20,33 @@
     }
 })();
 
+//Constructor de la clase Bar
+(function(){
+    self.Bar = function(x,y,width, height, board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+        
+    }
+
+    //Para mover las barras
+    self.Bar.prototype = {
+        down: function(){
+            this.y += this.speed;
+        },
+        up: function(){
+            this.y -= this.speed;
+        },
+        toString: function(){
+            return "x: "+this.x+" y: "+this.y;
+        }
+    }
+})();
+
 //Clase para dibujar las vistas del board
 (function(){
     self.BoardView = function(canvas,board){
@@ -28,6 +55,25 @@
         this.canvas.height = board.height;
         this.board = board;
         this.ctx = canvas.getContext("2d");
+    }
+
+    self.BoardView.prototype={
+        draw: function(){
+            for (var i = this.board.elements.length -1; i >= 0; i--) {
+                var el = this.board.elements[i];
+
+                draw(this.context,el);
+            };
+        }
+    },
+
+     //Funcion que dibuja el board
+     function draw(context,element){
+        switch(element.kind){
+            case "square":
+                context.fillRect(element.x,element.y, element.width,element.height);
+                break;
+        }
     }
 })();
 
